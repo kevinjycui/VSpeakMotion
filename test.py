a9b42928f0ce857d7aeffd09a3acd8bb7c886382
kevinjycui <kevincui1230@gmail.com>
2023-06-10 14:19:08 -0400

    Spectrogram test

---
+++ test.py
@@ -0,0 +1,13 @@
+import torch
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
+
+print(torch.__version__)
+print(torchaudio.__version__)
+
+import librosa
+import matplotlib.pyplot as plt
+
+from IPython.display import Audio
+from torchaudio.utils import download_asset
